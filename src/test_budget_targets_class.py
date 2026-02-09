4b9b93639f009a06d00b4dffa1f7b3ec7a658731
hillarymuir <hillarymuir@pm.me>
2026-02-09 14:25:37 -0500

    test: add tests for bt class functions

--- src/test_budget_targets_class.py
+++ src/test_budget_targets_class.py
@@ -35,10 +35,46 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(bt_class_instance.target_dict, {"key":"value"})
         self.assertEqual(file_content_str, "{\"key\":\"value\"}")
 
-    def test_bt_class_read(self):
+    def test_bt_class_get(self):
         """Test get_targets function of BT class"""
         bt_class_instance = bt_class.BudgetTargets(target_dict={"key": "value"})
 
         file_contents_dict = bt_class_instance.get_targets()
         
         self.assertEqual(file_contents_dict, {"key": "value"})
+
+    def test_bt_class_delete(self):
+        """Test delete_target function of BT class with a key that exists"""
+        bt_class_instance = bt_class.BudgetTargets(target_dict={"key": "value"})
+
+        bt_class_instance.delete_target("key")
+
+        file_contents_dict = bt_class_instance.get_targets()
+        
+        self.assertEqual(file_contents_dict, {})
+
+    def test_bt_class_delete_does_not_exist(self):
+        """Test delete_target function of BT class with a key that does not exist"""
+        bt_class_instance = bt_class.BudgetTargets(target_dict={"key": "value"})
+
+        self.assertRaises(KeyError, bt_class_instance.delete_target("bad_key"))
+
+    def test_bt_class_add(self):
+        """Test add_or_change_target function of BT class for adding a target"""
+        bt_class_instance = bt_class.BudgetTargets(target_dict={"key": "value"})
+
+        bt_class_instance.add_or_change_target("key2", "value2")
+
+        file_contents_dict = bt_class_instance.get_targets()
+        
+        self.assertEqual(file_contents_dict, {"key": "value", "key2": "value2"})
+
+    def test_bt_class_change(self):
+        """Test add_or_change_target function of BT class for changing a target"""
+        bt_class_instance = bt_class.BudgetTargets(target_dict={"key": "value"})
+
+        bt_class_instance.add_or_change_target("key", "new_value")
+
+        file_contents_dict = bt_class_instance.get_targets()
+        
+        self.assertEqual(file_contents_dict, {"key": "new_value"})
