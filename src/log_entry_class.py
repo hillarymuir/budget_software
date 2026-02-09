786cc872e8dbc651368fbe5eb1baef43e645472d
hillarymuir <hillarymuir@pm.me>
2026-02-09 15:26:22 -0500

    refactor: make LogEntry attributes public and remove get()

--- src/log_entry_class.py
+++ src/log_entry_class.py
@@ -10,24 +10,10 @@ Class for monthly log object.
 class LogEntry:
     """Class for a budget log entry."""
     def __init__(self, date, source, category, dollar_amt):
-        self._date = date
-        self._source = source
-        self._category = category
-        self._dollar_amt = dollar_amt
+        self.date = date
+        self.source = source
+        self.category = category
+        self.dollar_amt = dollar_amt
 
     def __str__(self):
-        return f"{self.get("date")}, {self.get("source")}, {self.get("category")}, {self.get("dollar_amt")}"
-    
-    def get(self, characteristic):
-        """Get a log entry class variable"""
-        match characteristic:
-            case "date":
-                return self._date
-            case "source":
-                return self._source
-            case "category":
-                return self._category
-            case "dollar_amt":
-                return self._dollar_amt
-            case _:
-                raise NameError("Error: Characteristic not found")
+        return f"{self.date}, {self.source}, {self.category}, {self.dollar_amt}"
