1ca49547c345ca6164de6dedc014db13a1c693cd
hillarymuir <hillarymuir@pm.me>
2026-02-09 12:35:59 -0500

    test: update bt class tests to test .txt file

--- src/test_budget_targets_class.py
+++ src/test_budget_targets_class.py
@@ -16,13 +16,21 @@ class TestFunctions(unittest.TestCase):
     def test_bt_class_creation_empty(self):
         """Test creation of BT class with empty dict"""
         bt_class_instance = bt_class.BudgetTargets()
+
+        with open("../private/targets/curr_target.txt", mode="r", encoding="utf-8") as f:
+            file_content_str = f.read()
         
         self.assertIsInstance(bt_class_instance, bt_class.BudgetTargets)
         self.assertEqual(bt_class_instance.target_dict, {})
+        self.assertEqual(file_content_str, "{}")
 
     def test_bt_class_creation_with_dict(self):
         """Test creation of BT class with specified dict"""
         bt_class_instance = bt_class.BudgetTargets(target_dict={"key": "value"})
+
+        with open("../private/targets/curr_target.txt", mode="r", encoding="utf-8") as f:
+            file_content_str = f.read()
         
         self.assertIsInstance(bt_class_instance, bt_class.BudgetTargets)
         self.assertEqual(bt_class_instance.target_dict, {"key":"value"})
+        self.assertEqual(file_content_str, "{\"key\":\"value\"}")
