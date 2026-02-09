e7fc9ebf53fc50031a5e36692e10cb58d9a41721
hillarymuir <hillarymuir@pm.me>
2026-02-09 11:45:25 -0500

    test: add bt class creation tests

--- src/test_budget_targets_class.py
+++ src/test_budget_targets_class.py
@@ -8,8 +8,21 @@ Tests for functions in budget_targets_class.py.
 """
 
 import unittest
+import budget_targets_class as bt_class
 
 class TestFunctions(unittest.TestCase):
+    """Test budget_targets_class and its functions"""
 
-    def test(self):
-        pass
+    def test_bt_class_creation_empty(self):
+        """Test creation of BT class with empty dict"""
+        bt_class_instance = bt_class.BudgetTargets()
+        
+        self.assertIsInstance(bt_class_instance, bt_class.BudgetTargets)
+        self.assertEqual(bt_class_instance.target_dict, {})
+
+    def test_bt_class_creation_with_dict(self):
+        """Test creation of BT class with specified dict"""
+        bt_class_instance = bt_class.BudgetTargets(target_dict={"key": "value"})
+        
+        self.assertIsInstance(bt_class_instance, bt_class.BudgetTargets)
+        self.assertEqual(bt_class_instance.target_dict, {"key":"value"})
