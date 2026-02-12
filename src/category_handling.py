bd26f7b46222e1967bae70626aab2e4e9ee35ea5
hillarymuir <hillarymuir@pm.me>
2026-02-12 10:46:19 -0500

    plan: add structure and comments planning category handling implementation

---
+++ src/category_handling.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+Docstring for budget_software.src.category_handling.py
+
+Functions related to handling budget category lists
+
+"""
+
+import csv
+from pathlib import Path
+
+# file paths hardcoded relative to project root
+PROJECT_ROOT = Path(__file__).resolve().parents[1] 
+TARGETS_DIR = PROJECT_ROOT / "private" / "targets"
+TARGETS_FILE = TARGETS_DIR / "categories.csv"
+
+def add_category(category):
+    """Add category to list"""
+    pass
+
+def del_category(category):
+    """Delete category in list"""
+    # TODO: prompt user instead of just deleting all entries with that category
+    pass
+
+def edit_category(old_cat, new_cat):
+    """Edit category in list"""
+    # TODO: automatically change all entries that use the old category
+    pass
---
+++ src/test_category_handling.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Docstring for budget_software.src.category_handling
+
+Tests for functions in category_handling.py.
+
+"""
+
+import unittest
+import log_class
+import log_entry_class as le_class
+import budget_targets_class as bt_class
+import category_handling
+
+class TestFunctions(unittest.TestCase):
+    """Tests for functions in category_handling.py."""
+
+    def test_add_category(self):
+        """Test category addition"""
+        pass
+
+    def test_delete_category(self):
+        """Test category deletion"""
+        pass
+
+    def test_edit_category(self):
+        """Test category editing"""
+        pass
+
+    def test_budget_targets_category_creation(self):
+        """Test budget targets class's automatic category addition"""
+        pass
+
+    def test_log_class_category_creation(self):
+        """Test log class's automatic category addition"""
+        pass
