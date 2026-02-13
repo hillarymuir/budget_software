f4dc9a99399550d06c0d50d5681df3ad1a53d4b9
hillarymuir <hillarymuir@pm.me>
2026-02-13 11:21:40 -0500

    file mgmt: adjust src file structure

--- src/generate_spreadsheet.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-"""
-Docstring for budget_software.src.generate_spreadsheet
-
-Functions to create a spreadsheet that shows budget targets alongside actual expenditures (as calculated from logs).
-
-"""
