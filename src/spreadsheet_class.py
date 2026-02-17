bf0ce034a01417777f50321f9dd90763f71e568d
hillarymuir <hillarymuir@pm.me>
2026-02-17 14:14:09 -0500

    feat: initialize spreadsheet class

--- src/spreadsheet_class.py
+++ src/spreadsheet_class.py
@@ -6,3 +6,20 @@ Docstring for budget_software.src.spreadsheet_class
 Class for spreadsheet (which contains budget targets alongside budget expenditures.)
 
 """
+
+import log_class
+import budget_targets_class
+
+class Spreadsheet:
+    """Class that holds a budget spreadsheet for one month."""
+    def __init__(self, log_obj, targets_obj):
+        self.log = log_obj
+        self.targets = targets_obj
+        self.month = int(self.log.name)
+
+        # calculate money remaining
+        self.money_remaining = 0.0 # TODO: sum budget entries
+
+    def update_spreadsheet(self):
+        """Updates targets and totals based on log."""
+        pass
