4c03fde56ad94fa73b5e312924d46d2198b01d91
hillarymuir <hillarymuir@pm.me>
2026-02-17 14:35:29 -0500

    test: add spreadsheet creation test

--- src/test_spreadsheet_class.py
+++ src/test_spreadsheet_class.py
@@ -9,9 +9,23 @@ Tests for functions in spreadsheet_class.py.
 
 import unittest
 
-import spreadsheet_class
+import budget_targets_class as bt_class
+import log_entry_class as le_class
+import log_class
+import spreadsheet_class as sheet_class
 
 class TestFunctions(unittest.TestCase):
 
     def test_spreadsheet_creation(self):
-        pass
+        """Test creation of spreadsheet class"""
+        bt_class_instance = bt_class.BudgetTargets()
+        le_class_instance = le_class.LogEntry(20260101, "Source", "Category", 1.11)
+        log_class_instance = log_class.Log([le_class_instance])
+
+        spreadsheet = sheet_class.Spreadsheet(log_class_instance, bt_class_instance)
+        
+        self.assertIsInstance(spreadsheet, sheet_class.Spreadsheet)
+        self.assertIs(spreadsheet.log, log_class_instance)
+        self.assertIs(spreadsheet.targets, bt_class_instance)
+        self.assertEqual(spreadsheet.month, int(log_class_instance.name))
+        self.assertEqual(1.11, spreadsheet.money_remaining)
